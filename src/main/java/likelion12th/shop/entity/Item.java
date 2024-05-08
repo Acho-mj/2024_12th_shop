03da5dc2548df2627467b21ab2fd93a9e3b0e777
Choi MinJung <mj020503@naver.com>
2024-05-08 17:29:17 +0900

    feat: 주문 취소

--- src/main/java/likelion12th/shop/entity/Item.java
+++ src/main/java/likelion12th/shop/entity/Item.java
@@ -30,14 +30,11 @@ public class Item extends Base {
     private String itemImgPath;
 
     // 상품의 재고를 감소시키는 로직
-    public void removeStock(int stockNumber){
-        // 상품의 재고 수량에서 주문 후 남은 재고 수량을 구한다.
+    public void removeStock(int stock){
         int restStock = this.stock - stock;
         if(restStock<0){
-            // "상품의 재고 < 주문 수량"일 경우 재고 부족 예외 발생
             throw new OutOfStockException("상품의 재고가 부족 합니다. (현재 재고 수량: " + this.stock + ")");
         }
-        // 주문 후 남은 재고 수량을 상품의 현재 재고 값으로 할당한다.
         this.stock = restStock;
     }
 }
